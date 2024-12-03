f3fc1e203d2267faef6fbc1d8fb3b118478f8df5
Max <maksym@smartrr.com>
2024-12-03 18:50:41 +0200

    add product page added

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,13 @@
 exports.getProducts = (req, res, next) => {
-    res.render("shop")
+    res.render("shop", {
+        pageTitle: "Main page",
+        path: "/"
+    })
+}
+
+exports.getAddProduct = (req, res, next) => {
+    res.render("addProduct", {
+        pageTitle: "Add product",
+        path: "add-product"
+    })
 }
--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,8 @@ const shopControllers = require("../controllers/shop");
 
 const router = express.Router();
 
+router.get("/add-product", shopControllers.getAddProduct)
+
 router.get("/", shopControllers.getProducts)
 
 module.exports = router

b58618ddf829cf7c82056fbdecd8f30a635fcea3
Max <maksym@smartrr.com>
2024-12-04 21:30:04 +0200

    signup and login added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/user")
+const bcryptjs = require("bcryptjs")
 
 exports.postLogout = (req, res, next) => {
 
@@ -6,12 +7,19 @@ exports.postLogout = (req, res, next) => {
 
 exports.postSignUp = (req, res, next) => {
     const { email, password } = req.body;
-    // TODO hash user's password
-    const newUser = new User({ email, password })
-
     // TODO check if user exists
-    newUser.save()
-        .then(() => res.redirect("/"))
+
+    bcryptjs
+        .hash(password, 12)
+        .then(hashedPassword => {
+            const newUser = new User({ email, password: hashedPassword })
+
+            newUser.save()
+                .then(() => res.redirect("/login"))
+                .catch(() => {
+                    throw new Error()
+                })
+        })
         .catch(() => {
             throw new Error()
         })
@@ -25,7 +33,21 @@ exports.getSignUp = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
+    const { email, password } = req.body;
 
+    User.findOne({ email })
+        .then(user => {
+            bcryptjs.compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        return res.redirect("/")
+                    }
+                    return res.redirect("/login")
+                })
+        })
+        .catch(() => {
+            throw new Error()
+        })
 }
 
 exports.getLogin = (req, res, next) => {
